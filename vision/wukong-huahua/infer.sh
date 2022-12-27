313c7c3ebb779457815650e2a7a71d576594d6d1
Mr.  Little Zhu <zhumavip@163.com>
2022-12-27 17:26:58 +0800

    add dpm-solver
    add dpm-solver

--- vision/wukong-huahua/infer.sh
+++ vision/wukong-huahua/infer.sh
@@ -18,14 +18,7 @@
 export GLOG_v=3
 export ASCEND_GLOBAL_LOG_LEVEL=3
 export ASCEND_SLOG_PRINT_TO_STDOUT=0
+export DEVICE_ID=0
 
-python txt2img.py \
---prompt "来自深渊 风景 绘画 写实风格" \
---config configs/v1-inference-chinese.yaml \
---output_path ./output/ \
---seed 42 \
---n_iter 4 \
---n_samples 8 \
---W 512 \
---H 512 \
---ddim_steps 50 
+python txt2img.py --prompt "来自深渊 风景 绘画 写实风格" --config configs/v1-inference-chinese.yaml --output_path ./output/ --seed 42 --dpm_solver --n_iter 4 --n_samples 4 --W 512 --H 512 --ddim_steps 15
+#python txt2img.py --prompt "来自深渊 风景 绘画 写实风格" --config configs/v1-inference-chinese.yaml --output_path ./output/ --seed 42 --n_iter 4 --n_samples 4 --W 512 --H 512 --ddim_steps 30
