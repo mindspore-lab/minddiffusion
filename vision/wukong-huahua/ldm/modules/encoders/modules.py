6a6147677227a6e52ad397f6f2d8b5114f0e7174
chensida <chensida@huawei.com>
2023-03-25 11:21:40 +0800

    update model

---
+++ vision/wukong-huahua/ldm/modules/encoders/modules.py
@@ -0,0 +1,55 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import mindspore as ms
+import mindspore.nn as nn
+import mindspore.ops as ops
+from mindspore import Tensor
+from ldm.models.clip_zh.simple_tokenizer import WordpieceTokenizer
+from .text_encoder import TextEncoder
+
+def tokenize(texts):
+    tokenizer = WordpieceTokenizer()
+    
+    SOT_TEXT = "[CLS]"
+    EOT_TEXT = "[SEP]"
+    CONTEXT_LEN = 77
+
+    if isinstance(texts, str):
+        texts = [texts]
+
+    sot_token = tokenizer.encoder[SOT_TEXT]
+    eot_token = tokenizer.encoder[EOT_TEXT]
+    all_tokens = [[sot_token] + tokenizer.encode(text) + [eot_token] for text in texts]
+    result = ops.Zeros()((len(all_tokens), CONTEXT_LEN), ms.int64)
+
+    for i, tokens in enumerate(all_tokens):
+        if len(tokens) > CONTEXT_LEN:
+            tokens = tokens[:CONTEXT_LEN - 1] + [eot_token]
+
+        result[i, : len(tokens)] = Tensor(tokens)
+
+    return result
+
+class FrozenCLIPEmbedder_ZH(nn.Cell):
+    def __init__(self, max_length=77, use_fp16=False):
+        super(FrozenCLIPEmbedder_ZH, self).__init__()
+        self.dtype = ms.float16 if use_fp16 else ms.float32
+        self.max_length = max_length
+        self.transformer = TextEncoder(context_length=77, vocab_size=49408, output_dim=768, width=768, layers=12, heads=12, dtype=self.dtype)
+        self.transformer.set_train(False)
+
+    def construct(self, x):
+        output = self.transformer(x)
+        return output
